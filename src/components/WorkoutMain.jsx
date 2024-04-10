4d638d8c40d11f5ad752289e9e926097f5b2fc15
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-04-11 00:06:32 +0400

    change images

--- src/components/WorkoutMain.jsx
+++ src/components/WorkoutMain.jsx
@@ -6,42 +6,42 @@ const schedule = [
     day: 1,
     videos: [
         { link: "https://www.youtube.com/watch?v=_xrAGY9ST4g&ab_channel=ZannavanDijk", image: "aaa.png" },
-        { link: "https://www.youtube.com/watch?v=jtDp6C9zKzw&ab_channel=ZannavanDijk", image: "v1698754006/6_fdimtx.jpg" },
+        { link: "https://www.youtube.com/watch?v=jtDp6C9zKzw&ab_channel=ZannavanDijk", image: "bbb.png" },
     ]
 },
 {
     day: 2,
     videos: [
-        { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o&ab_channel=MimiKuo-Deemer", image: "v1698754006/7_kbkv7o.jpg" },
-        { link: "https://www.youtube.com/watch?v=L_xrDAtykMI&ab_channel=TomMerrick", image: "v1698754007/2_gmiqj3.jpg" },
+        { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o&ab_channel=MimiKuo-Deemer", image: "ccc.png" },
+        { link: "https://www.youtube.com/watch?v=L_xrDAtykMI&ab_channel=TomMerrick", image: "bbb.png" },
     ]
 },
 {
     day: 3,
     videos: [
-        { link: "https://www.youtube.com/watch?v=fjWp8iazB9I&ab_channel=CHRISTINADORNER", image: "v1698754006/4_zpuut6.jpg"},
-        { link: "https://www.youtube.com/watch?v=igtL2m8oH2I&ab_channel=fitbymik", image: "v1698754006/3_m7aufm.jpg" },
+        { link: "https://www.youtube.com/watch?v=fjWp8iazB9I&ab_channel=CHRISTINADORNER", image: "ddd.png"},
+        { link: "https://www.youtube.com/watch?v=igtL2m8oH2I&ab_channel=fitbymik", image: "aaa.png" },
     ]
 },
 {
     day: 4,
     videos: [
-        { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o&ab_channel=MimiKuo-Deemer", image: "v1698754006/7_kbkv7o.jpg" },
-        { link: "https://www.youtube.com/watch?v=1n2pDfD6aFo&ab_channel=CandaceCabreraTavino", image: "v1698760696/8_kj2avb.jpg" }, 
+        { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o&ab_channel=MimiKuo-Deemer", image: "aaa.png" },
+        { link: "https://www.youtube.com/watch?v=1n2pDfD6aFo&ab_channel=CandaceCabreraTavino", image: "eee.png" }, 
     ]
 },
 {
     day: 5,
     videos: [
-        { link: "https://www.youtube.com/watch?v=j64BBgBGNIU&ab_channel=ChloeTing", image: "v1707495631/9_wcmrff.jpg" },
-        { link: "https://www.youtube.com/watch?v=I9nG-G4B5Bs&ab_channel=ChloeTing", image: "v1707495132/8_dwlzvy.jpg" },
+        { link: "https://www.youtube.com/watch?v=j64BBgBGNIU&ab_channel=ChloeTing", image: "fff.png" },
+        { link: "https://www.youtube.com/watch?v=I9nG-G4B5Bs&ab_channel=ChloeTing", image: "jjj.png" },
     ]
 },
 {
     day: 6,
     videos: [
-        { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o&ab_channel=MimiKuo-Deemer", image: "v1698754006/7_kbkv7o.jpg" },
-        { link: "https://www.youtube.com/watch?v=1n2pDfD6aFo&ab_channel=CandaceCabreraTavino", image: "v1698760696/8_kj2avb.jpg" },
+        { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o&ab_channel=MimiKuo-Deemer", image: "aaa.png" },
+        { link: "https://www.youtube.com/watch?v=1n2pDfD6aFo&ab_channel=CandaceCabreraTavino", image: "bbb.png" },
         
     ]
 },
@@ -77,8 +77,8 @@ const WorkoutMain = () => {
                 const grammar = videoCount === 1 ? "session" : "sessions";
     
                 const videoLinks = videos.map(video => `
-                    <a href="${video.link}" target="_blank">
-                        <img src="${video.image}" alt="Workout Video Image"/>
+                    <a href="images/${video.link}" target="_blank">
+                        <img src="/images/${video.image}" alt="Workout Video Image"/>
                     </a>
                 `).join('');
     
