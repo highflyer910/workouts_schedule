54445d698d9ae9475535108c572e849fad8ca250
Thea Mushambadze <thea.mushambadze@gmail.com>
2026-05-09 03:08:16 +0400

    update

--- src/data/workoutData.js
+++ src/data/workoutData.js
@@ -4,47 +4,47 @@ export const workoutSchedule = [
     name: "Monday",
     videos: [
       { link: "https://www.youtube.com/watch?v=_xrAGY9ST4g", image: "fff.jpg", title: "Upper Body Blast" },
-      { link: "https://www.youtube.com/watch?v=jtDp6C9zKzw", image: "ddd.jpg", title: "Core Crusher" },
+      { link: "https://www.youtube.com/watch?v=jtDp6C9zKzw", image: "ddd.jpg", title: "Lower Body Focus" },
     ]
   },
   {
     day: 2,
     name: "Tuesday",
     videos: [
-      { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o", image: "eee.jpg", title: "Yoga Flow" },
-      { link: "https://www.youtube.com/watch?v=L_xrDAtykMI", image: "bbb.jpg", title: "Flexibility" },
+      { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o", image: "eee.jpg", title: "8 Brocades Qigong" },
+      { link: "https://www.youtube.com/watch?v=L_xrDAtykMI", image: "bbb.jpg", title: "Stretch & Flexibility" },
     ]
   },
   {
     day: 3,
     name: "Wednesday",
     videos: [
-      { link: "https://www.youtube.com/watch?v=fjWp8iazB9I", image: "ccc.jpg", title: "HIIT Session" },
-      { link: "https://www.youtube.com/watch?v=igtL2m8oH2I", image: "fff.jpg", title: "Full Body Burn" },
+      { link: "https://www.youtube.com/watch?v=fjWp8iazB9I", image: "ccc.jpg", title: "Abs Session" },
+      { link: "https://www.youtube.com/watch?v=igtL2m8oH2I", image: "fff.jpg", title: "Booty Band Workout" },
     ]
   },
   {
     day: 4,
     name: "Thursday",
     videos: [
-      { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o", image: "eee.jpg", title: "Active Recovery" },
-      { link: "https://www.youtube.com/watch?v=1n2pDfD6aFo", image: "jjj.jpg", title: "Stretch & Relax" },
+      { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o", image: "eee.jpg", title: "8 Brocades Qigong" },
+      { link: "https://www.youtube.com/watch?v=1n2pDfD6aFo", image: "jjj.jpg", title: "Standing Yoga" },
     ]
   },
   {
     day: 5,
     name: "Friday",
     videos: [
-      { link: "https://www.youtube.com/watch?v=j64BBgBGNIU", image: "fff.jpg", title: "Leg Day" },
-      { link: "https://www.youtube.com/watch?v=I9nG-G4B5Bs", image: "aaa.jpg", title: "Glute Pump" },
+      { link: "https://www.youtube.com/watch?v=j64BBgBGNIU", image: "fff.jpg", title: "Toned Arms" },
+      { link: "https://www.youtube.com/watch?v=I9nG-G4B5Bs", image: "aaa.jpg", title: "Toned legs" },
     ]
   },
   {
     day: 6,
     name: "Saturday",
     videos: [
-      { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o", image: "eee.jpg", title: "Weekend Yoga" },
-      { link: "https://www.youtube.com/watch?v=1n2pDfD6aFo", image: "jjj.jpg", title: "Evening Stretch" },
+      { link: "https://www.youtube.com/watch?v=3K-0JpiJu-o", image: "eee.jpg", title: "8 Brocades Qigong" },
+      { link: "https://www.youtube.com/watch?v=1n2pDfD6aFo", image: "jjj.jpg", title: "Standing Yoga" },
     ]
   },
 ];
